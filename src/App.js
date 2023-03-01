e484d06a17b7bf7b8e86b70eceb50f972a222540
Tatva Kamdar <kamdartatv1@gmail.com>
2023-03-01 14:44:44 +0530

    Made Navbar Partially

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Navbar from "./Views/Navbar";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen font-playfair text-5xl font-semibold bg-gradient-to-b from-orange-500 via-green-500 to-cyan-600">
-      Hi
-    </div>
+    <Navbar/>
   );
 }
 
