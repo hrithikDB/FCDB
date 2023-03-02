5d8e733bdeb47399775afc5bd31b5b3418a65ca0
Aman Jolly <aman.amanjolly@gmail.com>
2023-03-02 15:08:18 +0530

    Basic Backend Setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,18 @@ const PORT = process.env.PORT || 8080;
 
 app.set("port", PORT);
 
+// Parsing the request parameters
+app.use(express.urlencoded());
+app.use(express.static('./assests'));
+
+//Set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+//Use express router 
+app.use('/',require('./routes/index.js'));
+
 app.listen(PORT, () => {
   console.log("Yay");
 });
