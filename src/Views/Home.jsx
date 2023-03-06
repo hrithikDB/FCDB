bb8eb4c2e5887e721dd5552ddc55442709baa06d
Tatva Kamdar <kamdartatv1@gmail.com>
2023-03-06 12:51:23 +0530

    Made Home Page

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Searchbar from "../Components/Searchbar";
+import ServiceCard from "../Components/ServiceCard";
 
 const Home = () => {
   return (
@@ -30,7 +31,7 @@ const Home = () => {
       </div>
       <Searchbar />
 
-      <div className="bg-white">
+      <div className="bg-white text-blue">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto text-center pb-12">
             <h2 className="text-3xl font-extrabold text-gray-900">
@@ -38,50 +39,24 @@ const Home = () => {
             </h2>
           </div>
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            {/* Service cards */}
-            <div className="bg-white shadow-md rounded-lg hover:shadow-lg transition duration-300">
-              <div className="p-4">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                  Logo design
-                </div>
-                <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-                >
-                  Creative logo design for your business
-                </a>
-                <p className="mt-2 text-gray-500">Starting at $50</p>
-              </div>
-            </div>
-            <div className="bg-white shadow-md rounded-lg hover:shadow-lg transition duration-300">
-              <div className="p-4">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                  Website development
-                </div>
-                <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-                >
-                  Custom website development for your business
-                </a>
-                <p className="mt-2 text-gray-500">Starting at $500</p>
-              </div>
-            </div>
-            <div className="bg-white shadow-md rounded-lg hover:shadow-lg transition duration-300">
-              <div className="p-4">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                  Social media marketing
-                </div>
-                <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-                >
-                  Expert social media marketing services for your business
-                </a>
-                <p className="mt-2 text-gray-500">Starting at $250</p>
-              </div>
-            </div>
-            {/* Add more service cards here */}
+            <ServiceCard
+              ServiceName={"Website development"}
+              Link={"/"}
+              Description={"Custom website development for your business"}
+              StartingPrice={50}
+            />
+            <ServiceCard
+              ServiceName={"Website development"}
+              Link={"/"}
+              Description={"Custom website development for your business"}
+              StartingPrice={50}
+            />
+            <ServiceCard
+              ServiceName={"Website development"}
+              Link={"/"}
+              Description={"Custom website development for your business"}
+              StartingPrice={50}
+            />
           </div>
         </div>
       </div>
--- src/Views/Routing.jsx
+++ src/Views/Routing.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import Home from "./Home";
-import About from "./About";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Routing = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <Home />;
 };
 
 export default Routing;
